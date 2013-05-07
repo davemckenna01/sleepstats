89f69d68e9d7c0dc49ff7a5b05c7f2dc5bac1697
Dave McKenna <davemckenna01@gmail.com>
2013-05-06 23:17:26 -0400

    add homepage msg logic

--- djangoproject/fitbyte/views.py
+++ djangoproject/fitbyte/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 def home(request):
-
-    context = {'message': ''}
+    context = {
+        'fitbit': True if 'fitbit_access_token' in request.session else False
+    }
     return render(request, 'home.html', context)
 
