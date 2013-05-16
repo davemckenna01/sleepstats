967877b9e70f1c92ffc298b19c6b170758cad796
Dave McKenna <davemckenna01@gmail.com>
2013-05-15 23:24:22 -0400

    get cooking with angular

--- fitbyte_ng/app/scripts/controllers/main.js
+++ fitbyte_ng/app/scripts/controllers/main.js
@@ -1,10 +1,8 @@
 'use strict';
 
 angular.module('fitbyteApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, apis) {
+    console.log(apis);
+    $scope.fitbitAuthorized = apis.authorized.fitbit;
+    $scope.fitbitURLs = apis.urls.fitbit;
   });
