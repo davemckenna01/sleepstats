a73366c884d1a8e294a77c410d6c18cd98f7ee71
Dave McKenna <davemckenna01@gmail.com>
2013-08-10 13:45:57 -0400

    another try

--- djangoproject/sleepstats/settings.py
+++ djangoproject/sleepstats/settings.py
@@ -69,7 +69,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '../staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
