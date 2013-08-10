4d5a8614e848371595898e77c8878772cb8b2094
Dave McKenna <davemckenna01@gmail.com>
2013-08-10 13:42:28 -0400

    fix static files path

--- djangoproject/sleepstats/settings.py
+++ djangoproject/sleepstats/settings.py
@@ -77,7 +77,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, "../../sleepstats_ng"), 
+    os.path.join(PROJECT_ROOT, "../../sleepstats_ng/"), 
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
