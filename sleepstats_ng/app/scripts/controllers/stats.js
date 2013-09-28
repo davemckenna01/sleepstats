53c7abbb4c99e1a0d26cb34552de0ed0c91d9604
Dave McKenna <davemckenna01@gmail.com>
2013-09-28 01:14:41 -0400

    redirect to fitbit oauth if not approved yet

--- sleepstats_ng/app/scripts/controllers/stats.js
+++ sleepstats_ng/app/scripts/controllers/stats.js
@@ -1,18 +1,25 @@
 'use strict';
 
 angular.module('sleepstatsApp')
-  .controller('StatsCtrl', function ($scope, $http, $q, apis) {
+  .controller('StatsCtrl', function ($scope, $http, $q, apis, $window) {
     $q.all([
       $http.get(apis.urls.fitbit.sleepAwakenings),
       $http.get(apis.urls.fitbit.sleepTimeToSleep),
       $http.get(apis.urls.fitbit.sleepTimeInBed)
     ])
-    .then(function(data) {
+    .then(
+    function(data) {
       $scope.awakenings = data[0].data['sleep-awakeningsCount'];
       $scope.timeToSleep = data[1].data['sleep-minutesToFallAsleep'];
       $scope.timeInBed = data[2].data['sleep-timeInBed'];
       $scope.dataLoaded = true;
-    });
+    },
+    function(reason) {
+      if (reason.status && reason.status === 401) {
+        $window.location.href = apis.urls.fitbit.auth;
+      }
+    }
+    );
 
     $scope.dataLoaded = false;
 
