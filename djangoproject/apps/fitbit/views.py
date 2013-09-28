001342c16a609a771f0c99877694040f868246bc
Dave McKenna <davemckenna01@gmail.com>
2013-09-28 15:19:26 -0400

    remove simulated latency

--- djangoproject/apps/fitbit/views.py
+++ djangoproject/apps/fitbit/views.py
@@ -8,31 +8,26 @@ import pprint
 api_url = "http://api.fitbit.com/1/user/-/"
 
 def sleep_by_date(request, date):
-    time.sleep(1)
     url = api_url + "sleep/date/" + date + ".json"
 
     return oauth1_utils.make_api_call(url, request, API_CONFIG)
 
 def sleep_awakenings(request, start, end):
-    time.sleep(1)
     url = api_url + "sleep/awakeningsCount/date/" + start + "/" + end + ".json"
 
     return oauth1_utils.make_api_call(url, request, API_CONFIG)
 
 def sleep_time_to_sleep(request, start, end):
-    time.sleep(1)
     url = api_url + "sleep/minutesToFallAsleep/date/" + start + "/" + end + ".json"
 
     return oauth1_utils.make_api_call(url, request, API_CONFIG)
 
 def sleep_time_in_bed(request, start, end):
-    time.sleep(1)
     url = api_url + "sleep/timeInBed/date/" + start + "/" + end + ".json"
 
     return oauth1_utils.make_api_call(url, request, API_CONFIG)
 
 def sleep_start_time(request, start, end):
-    time.sleep(1)
     url = api_url + "sleep/startTime/date/" + start + "/" + end + ".json"
 
     return oauth1_utils.make_api_call(url, request, API_CONFIG)
