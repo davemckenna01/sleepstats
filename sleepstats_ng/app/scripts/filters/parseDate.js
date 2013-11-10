d1c93a6957ef8fcaa2bcbfc996b3c3fa33920598
Dave McKenna <davemckenna01@gmail.com>
2013-11-10 10:39:33 -0500

    move default text formatting into filter code

--- sleepstats_ng/app/scripts/filters/parseDate.js
+++ sleepstats_ng/app/scripts/filters/parseDate.js
@@ -12,7 +12,7 @@ angular.module('sleepstatsApp')
       if (date) {
         return date.toString(dateFormat);
       } else {
-        return 'e.g. "' + defaultText + '"';
+        return defaultText;
       }
     };
   });
